50317ab6165a2cb598382dbd76446f8e3943d1e3
yuri morais <145020646+trrstddzst@users.noreply.github.com>
2026-03-23 13:53:27 +0000

    versão1

---
+++ temperatura.js
@@ -0,0 +1,24 @@
+//Conversão de temperturas
+
+const cidade ="Curitiba";
+const tempCelsius = 18.5;
+
+const tempFahrenheit =temCelsiu * (9/5)+ 32;
+const tempKelvin = temCelsiu + 273.15;
+const variacaoDiaria = 26.3 - temCelsiu;
+
+console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}.`);
+console.log(`Celsius: ${tempCelsius}`);
+
+//Fazer console.log para Fahrenheit e para Kelvin
+
+const tempFahrenheit tempCelsius (9/5) + 32
+const tempKelvin tempCelsius + 273.15;
+const variacaoDiaria = 26.3 - tempCelsius;
+
+console.log(Temperatura em ${cidade) hoje está ${tempCelsius}.`);
+console.log('Celsius: ${tempCelsius) Cº);
+console.log(Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº);
+console.log(Kelvin: ${})
+//Tecla Alt Gr + tecla colchetes para fazer o
+
