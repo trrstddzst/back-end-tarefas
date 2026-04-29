56b7210d7bbf78f88d969a3d1cc2dfda74901163
yuri morais <145020646+trrstddzst@users.noreply.github.com>
2026-04-29 12:22:46 +0000

    versao2

---
+++ objeto.js
@@ -0,0 +1,23 @@
+const bancoAutomotivo = {
+    material = "Couro natural",
+    cor = "Caramelo"
+    tamanho = "Padrão"
+    concluido = false
+};
+
+//console.log("Material escolhido: ", bancoAutomotivo.material);
+
+//bancoAutomotivo.concluido  = true
+//console.log(bancoAutomotivo)
+
+
+//Loja de roupas
+
+const vestido = {
+    cor = marrom
+    material = algodão
+    tamanho = pp
+    valor = 80
+    feito = false
+   
+}
